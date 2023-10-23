dc8238d2259cd45479b4d063058794cf0f750452
hyrumnoble <hyrumnoble3@gmail.com>
2023-10-23 22:20:22 +0200

    final

--- chamber/scripts/weather.js
+++ chamber/scripts/weather.js
@@ -16,3 +16,25 @@ window.weatherWidgetConfig.push({
     s.setAttribute('data-timestamp', +new Date());
     (d.head || d.body).appendChild(s);
 })();
+function calculateWindChill(temperature, windSpeed) {
+    
+    if (temperature <= 50 && windSpeed > 3.0) {
+      const windChill = calculateWindChillIndex(temperature, windSpeed);
+      return `Wind Chill: ${windChill.toFixed(1)} °F`;
+    } else {
+      return "N/A";
+    }
+  }
+  
+  function calculateWindChillIndex(temperature, windSpeed) {
+    const windChill = 35.74 + 0.6215 * temperature - 35.75 * Math.pow(windSpeed, 0.16) + 0.4275 * temperature * Math.pow(windSpeed, 0.16);
+    return windChill;
+  }
+  
+  
+  const temperature = 32; 
+  const windSpeed = 10;  
+  const windChillValue = calculateWindChill(temperature, windSpeed);
+  
+
+  document.querySelector('.wind-chill').textContent = windChillValue;
